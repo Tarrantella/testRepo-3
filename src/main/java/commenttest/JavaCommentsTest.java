f99b77c9fc97922503df0372405b590a95ce70f3
Tarrantella <user9fut@some.mail>
2017-05-16 07:53:56 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1493909284356
+//first_change1494919817060
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
