03a8705532a3e43659a39659990cf599248649af
Tarrantella <iedexmain1@gmail.com>
2017-06-12 13:18:43 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1496907278926
+//first_change1497268831234
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
