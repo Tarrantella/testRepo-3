027936396cadec459b6c8ca2c8fe4f007a7fdaff
Tarrantella <mmusienko@codenvy.com>
2017-06-18 21:12:51 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1497799665940
+//first_change1497813352376
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
