2a4eeae7a847dbd9d6c37064d17f822ae898052c
Tarrantella <useryhc3bc@some.mail>
2017-08-26 01:21:43 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1500589258999
+//first_change1503701799408
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
