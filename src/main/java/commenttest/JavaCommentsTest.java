ed2290e165a8e080f8e96170239c6d286ae3a9a8
Tarrantella <che@eclipse.org>
2017-11-20 00:16:16 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1510910510464
+//first_change1511134429361
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
